156ac17ac74c7dc7b977c6c9882d93c2025aae8e
fansters <edmundseizentals@gmail.com>
2022-10-31 22:50:13 +0200

    added custom lightbox, just need to tweek the functionality.

--- src/container/Product/Product.jsx
+++ src/container/Product/Product.jsx
@@ -1,53 +1,30 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { data, images } from "../../constants";
 
 import "./Product.css";
 
 const Product = () => {
-	const modalRef = useRef();
 	let [index, setIndex] = useState(0);
-	// const [slideIndex, setSlideIndex] = useState(1)
+	const [isOpen, setIsOpen] = useState(false);
 
-	function openModal() {
-		modalRef.style.display = "block";
-	}
-	// Close the Modal
-	async function closeModal() {
-		document.getElementById("myModal").style.display = "none";
-	}
-	showSlides(index);
+	const toggleIsOpen = () => {
+		setIsOpen(!isOpen);
+	};
 
-	// Next/previous controls
-	function plusSlides(n) {
-		showSlides((index += n));
-	}
-	function showSlides(n) {
-		var i;
-		var slides = document.getElementsByClassName("mySlides");
-		var dots = document.getElementsByClassName("demo");
-		if (n > slides.length) {
-			index = 1;
-		}
-		if (n < 1) {
-			index = slides.length;
-		}
-		for (i = 0; i < slides.length; i++) {
-			slides[i].style.display = "none";
-		}
-		for (i = 0; i < dots.length; i++) {
-			dots[i].className = dots[i].className.replace(" active", "");
-		}
-		// slides[index - 1].style.display = "block";
-		// dots[index - 1].className += " active";
-		// captionText.innerHTML = dots[index - 1].alt;
-	}
 	return (
 		<>
 			<div className='app__product'>
 				<div className='app__product-container'>
 					<div className='app__product-images'>
-						<div className='app__product-img' onClick={() => console.log(openModal())}>
-							<img src={images.bigImgs[index]} alt='Shoe' className='big-image' onClick={openModal()} />
+						<div className='app__product-img' onClick={toggleIsOpen}>
+							<img src={images.bigImgs[index]} alt='Shoe' className='big-image' onClick='' />
+						</div>
+						<div className='lightbox-modal'>
+							{isOpen ? (
+								<div onClick={toggleIsOpen} className='lightbox-div'>
+									<img src={images.bigImgs[index]} alt='lightbox shoe' className='lightbox-img' />
+								</div>
+							) : null}
 						</div>
 						<div className='app__product-thumbnail'>
 							{images.smallImgs?.map((item, i) => (
@@ -63,24 +40,7 @@ const Product = () => {
 							))}
 						</div>
 					</div>
-					{/* Lightbox pics */}
-					<div ref={modalRef} id='myModal' className='app__modal'>
-						<span className='close cursor' onclick={closeModal()}>
-							&times;
-						</span>
-						<div className='modal-content'>
-							<div className='mySlides'>
-								<img src={images.bigImgs[index]} alt='shoes' onClick={openModal()} />
-							</div>
-							{/* <!-- Next/previous controls --> */}
-							<a href='/' className='prev' onclick={plusSlides(1)}>
-								&#10094;
-							</a>
-							<a href='/' className='next' onclick={plusSlides(1)}>
-								&#10095;
-							</a>
-						</div>
-					</div>
+
 					<div className='app__product-desc'>
 						<p className='app__company-title'>{data.shoes[0].company}</p>
 						<h2 className='app__product-title'>{data.shoes[0].title}</h2>
@@ -116,12 +76,6 @@ const Product = () => {
 					</div>
 				</div>
 			</div>
-			<footer>
-				<div className='attribution'>
-					Coded by <br />
-					<a href='https://www.frontendmentor.io/profile/Fansters'>Fansters</a>
-				</div>
-			</footer>
 		</>
 	);
 };
