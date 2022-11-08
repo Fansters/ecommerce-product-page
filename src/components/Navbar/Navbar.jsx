25bb67487fdb7eff8b753b27d522fe798808ad2c
fansters <edmundseizentals@gmail.com>
2022-11-08 16:34:45 +0200

    added cart with items in it. data is still static. need to implement dynamic data

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -65,18 +65,36 @@ const Navbar = () => {
 			</div>
 			<div className='app__cta-btns'>
 				<div className='app__cta-cart'>
-					<button type='button' onMouseEnter={() => setShowCart(true)}>
-						<span className='cart-item-qty'>1</span>
+					<button type='button' onClick={() => setShowCart(true)}>
+						{totalQuantities > 0 && <span className='cart-item-qty'>{totalQuantities}</span>}
 						<img src={images.cartIcon} alt='cart' />
 					</button>
 					{showCart && (
-						<div className='app__cart-info' ref={cartRef} onMouseLeave={() => setShowCart(false)}>
+						<div className='app__cart-info' ref={cartRef} onClick={() => setShowCart(false)}>
 							<div className='app__cart-title'>
 								<p className='app__cart-heading'>Cart</p>
 							</div>
 							<div className='app__cart-items'>
 								<div className='app__cart-items-cont'>
-									{cartItems.length < 1 && <div className='empty-cart'>Your cart is empty.</div>}
+									{/* {cartItems.length < 1 && <div className='empty-cart'>Your cart is empty.</div>} */}
+									<div className='item-in-cart'>
+										<div className='item-img-cont'>
+											<img className='item-img' src={images.product1T} alt='shoe thumbnail' />
+										</div>
+										<div className='item-desc'>
+											<h3 className='item-title'>Full Limited Edition Sneakers</h3>
+											<div className='item-price-cont'>
+												<span className='total-qty'>$125.00 x 3 </span>
+												<span className='total-price'>$375.00</span>
+											</div>
+										</div>
+										<button type='button' className='item-delete'>
+											<img src={images.deleteIcon} alt='delete' />
+										</button>
+									</div>
+                           <button type='button' className='in-cart-btn'>
+                              Checkout
+                           </button>
 								</div>
 							</div>
 						</div>
